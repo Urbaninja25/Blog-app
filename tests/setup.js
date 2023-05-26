ee148ae1e764408e5542eb323c670f76e9e7017b
Urbaninja25 <nbeka17@freeuni.edu.ge>
2023-05-26 15:02:16 +0400

    final code

--- tests/setup.js
+++ tests/setup.js
@@ -2,13 +2,16 @@ jest.setTimeout(20000);
 require("../models/User");
 
 const mongoose = require("mongoose");
-const keys = require("../config/keys");
+// const keys = require("../config/keys");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://nugi:egeosi.@advancednode.n6r6fnt.mongodb.net/advnode?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 afterAll(async () => {
   await mongoose.disconnect();
